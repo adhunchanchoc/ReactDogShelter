43476eeddca5a682790062c12242bb5b8f7003a2
adhunchanchoc <adhunchanchoc@gmail.com>
2023-03-19 20:18:53 +0100

    zpracovano routovani pomoci HashRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import React, { Suspense } from "react";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
 
 function App() {
-  return <p>testovaci</p>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* cesta a prvek k vykresleni */}
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
